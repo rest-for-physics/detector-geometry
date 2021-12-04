818ec099cea3ba0b4d37e7c85bb482798083a916
Luis Obis <luis.antonio.obis@gmail.com>
2021-12-04 20:56:27 +0100

    Removed unused materials from https://github.com/mipt-npm/gdml.kt/commit/bfd113f01147c499457c084898618315a2dd4b7f. Currently needs to use local gdml.kt artifact until new release

--- generator/src/main/kotlin/Main.kt
+++ generator/src/main/kotlin/Main.kt
@@ -25,7 +25,7 @@ fun main() {
 
         for ((name, gdml) in geometries) {
             File("$outputDirectoryName/$geometryName/$name.gdml")
-                .writeText(gdml.encodeToString())
+                .writeText(gdml.removeUnusedMaterials().encodeToString())
         }
     }
 
